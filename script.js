6267a1911e180eec84d5902e934d66cfe4963aab
Cody <codyratcliffep@gmail.com>
2023-01-30 10:19:04 -0700

    add pseudo code to have a plan

--- script.js
+++ script.js
@@ -40,3 +40,18 @@ calculator.addEventListener("click", function(event) {
         console.log(stack);
     }
 });
+
+/* 
+I think how I should make this is have 3 variables: a - the first value, b - the operator, and c - the last value. From there I will evaluate the equation. The process should be that you can continue to input numbers but if you press an operator button it commits the value for a and the operator and moves the entering the values for c until you press the equal button.
+
+- Listen for number inputs and store them in variable a
+- If an operator button is pressed store the a value and b value and move on to c
+- listen for number inputs for the c value
+- if the equal button is pressed evaluate the equation
+
+This would look something like this at the end:
+let a = 25
+let b = *
+let c = 5
+console.log(a b c)
+*/
